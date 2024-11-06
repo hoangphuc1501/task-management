2d595cb89e2f369a3811001ec42a3b9818236f69
hoangphuc1501 <katakurilancer.com>
2024-11-06 18:25:33 +0700

    Sắp xếp theo tiêu chí

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -4,10 +4,18 @@ module.exports.index = async (req, res) => {
     const find = {
         deleted: false
     }
+    // bộ lọc trạng thái
     if(req.query.status){
         find.status = req.query.status
     }
-    const tasks = await Task.find(find);
+    // hết bộ lọc trạng thái
+    // sắp xếp theo tiêu chí
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey] = req.query.sortValue
+    }
+    // hết sắp xếp theo tiêu chí
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks)
 }
 
