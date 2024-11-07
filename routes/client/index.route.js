636934ad7c814341a3913c5bcdece9aecda4285c
hoangphuc1501 <katakurilancer.com>
2024-11-07 19:26:46 +0700

    route private

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,12 @@
 const taskRoute = require("./task.route");
 const userRoute = require("./user.route");
 
+const userMiddleware = require("../../middlewares/client/auth.middleware");
+
 module.exports = (app) =>{
-    app.use("/tasks", taskRoute)
+
+    app.use("/tasks", 
+        userMiddleware.requireAuth,
+        taskRoute)
     app.use("/users", userRoute)
 }
